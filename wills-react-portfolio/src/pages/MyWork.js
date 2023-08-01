4a693d482dc781955e8672f563b62c08eb0c591e
Masterchef842 <robertwilliamlord55@gmail.com>
2023-08-01 00:24:23 -0500

    end ofnight commit

--- wills-react-portfolio/src/pages/MyWork.js
+++ wills-react-portfolio/src/pages/MyWork.js
@@ -3,7 +3,7 @@ import TMF from "../assets/TMF.png";
 import JATE from "../assets/JATE.png";
 import SMT from "../assets/SMT.png";
 import WWWOT from "../assets/WWWOT.png";
-import down from  "../assets/down-arrow.png";
+import down from "../assets/down-arrow.png";
 import ShowcaseCard from "../components/ShowcaseCard/ShowcaseCard";
 
 import {
@@ -26,23 +26,26 @@ function Showcase() {
   const projData = [
     {
       title: "Taskmaster-Flex",
-      projectDesc: "A gameified habit tracking app where progress you make on your chores/habits/responsibilities translates into progress in a RPG style dungeon crawler, built using React, PhaserJS, MongoDB, Express and GraphQL",
+      projectDesc:
+        "A gameified habit tracking app where progress you make on your chores/habits/responsibilities translates into progress in a RPG style dungeon crawler, built using React, PhaserJS, MongoDB, Express and GraphQL",
       image: TMF,
       url: "https://taskmaster-flex-be1c20d82d58.herokuapp.com/",
       git: "https://github.com/Meister7K/TaskMaster-Flex",
-      featured: true
+      featured: true,
     },
     {
       title: "Spaceship Mission Tracker",
-      projectDesc: "A website for tracking upcoming spaceship launches by various agencies across the world. Includes monitoring of weather conditions at a launch site, information about the rocket and a countdown until launch, Built using Javascript, Express, and Swift  ",
+      projectDesc:
+        "A website for tracking upcoming spaceship launches by various agencies across the world. Includes monitoring of weather conditions at a launch site, information about the rocket and a countdown until launch, Built using Javascript, Express, and Swift  ",
       image: SMT,
       url: "https://masterchef842.github.io/Spaceship-Mission-Tracker/",
       git: "https://github.com/Masterchef842/Spaceship-Mission-Tracker",
-      featured: true
+      featured: true,
     },
     {
       title: "Wild World of Tech Blog",
-      projectDesc: "A social media blog and API complete with functionality for posting, liking, commenting, login/logout, and cookies. Built using handlebarsJS , MySQL, Sequelize, and Express ",
+      projectDesc:
+        "A social media blog and API complete with functionality for posting, liking, commenting, login/logout, and cookies. Built using handlebarsJS , MySQL, Sequelize, and Express ",
       image: WWWOT,
       url: "https://willswildworldoftech.herokuapp.com/",
       git: "https://github.com/Masterchef842/WillsWildWorldOfTech",
@@ -51,98 +54,113 @@ function Showcase() {
     },
     {
       title: "JA Text Editor PWA",
-      projectDesc: "An online text editor with Progressive Web App capabilities. Built using Webpack",
+      projectDesc:
+        "An online text editor with Progressive Web App capabilities. Built using Webpack",
       image: JATE,
       url: "https://worldsbesttexteditor.herokuapp.com/",
       git: "https://github.com/Masterchef842/WorldsBestTextEditor",
-      featured: true
+      featured: true,
     },
   ];
 
   const renderProjects = () => {
     return projData.map((project) => {
-        if(project.featured){
-            return (
-                <Carousel.Item key={project.title}>
-                  <a href={project.url || project.git}>
-                    <img src={project.image} style={{ height: "55vh" }} />
-                  </a>
-                  <Carousel.Caption
-                    style={{
-                      color: project.textColor || "white",
-                      fontWeight: "bold",
-                      fontSize: "200%",
-                      textShadow: project.textColor ? "none" : "4px 4px 4px black",
-                    }}
-                  >
-                    {project.title}
-                  </Carousel.Caption>
-                </Carousel.Item>
-              );
-
-        }
-        return <></>
-      
+      if (project.featured) {
+        return (
+          <Carousel.Item key={project.title}>
+            <a href={project.url || project.git}>
+              <img src={project.image} style={{ height: "55vh" }} />
+            </a>
+            <Carousel.Caption
+              style={{
+                color: project.textColor || "white",
+                fontWeight: "bold",
+                fontSize: "200%",
+                textShadow: project.textColor ? "none" : "4px 4px 4px black",
+              }}
+            >
+              {project.title}
+            </Carousel.Caption>
+          </Carousel.Item>
+        );
+      }
+      return <></>;
     });
   };
 
-  const renderCards=()=>{
-    return (projData.map((proj ,i)=>{
-        return (<ShowcaseCard key={proj.title} project={proj}/>) 
-        
-    }));
-  }
+  const renderCards = () => {
+    return projData.map((proj, i) => {
+      return <ShowcaseCard key={proj.title} project={proj} />;
+    });
+  };
 
   return (
     <>
-    
-    <Container style={{height: "90vh"}}fluid>
-        
-    
-       
-   
-    
-      <Row>
-      <h2>FEATURED PROJECTS</h2> 
-        <Col xs={6}>
-          <Carousel
-            activeIndex={index}
-            onSelect={handleSelect}
-            
-            style={{top:"20vh", border: "solid 5px black" }}
-          >
-            {renderProjects()}
-          </Carousel>
-        </Col>
-        <Col xs={6}>
-          <Card  style={{ top:"20vh",height: "56vh", alignItems: "center"}}>
-            <Card.Body>
-              <Card.Header><Card.Title>{projData[index].title}</Card.Title></Card.Header>
-              <Card.Text>{projData[index].projectDesc}</Card.Text>
-            </Card.Body>
-            <Row className="justify-content-between" style={{bottom: "10px"}}>
+      <Container style={{ height: "90vh" }} fluid>
+        <Row>
+          <Row>
+            <Col>
+              <h2 style={{position:'realative', background: "#496DDB", marginTop: "20vh", border: "solid 5px black" }}>
+                FEATURED PROJECTS
+              </h2>
+            </Col>
+          </Row>
+
+          <Col xs={6}>
+            <Carousel
+              activeIndex={index}
+              onSelect={handleSelect}
+              style={{ border: "solid 5px black" }}
+            >
+              {renderProjects()}
+            </Carousel>
+          </Col>
+          <Col xs={6}>
+            <Card border="dark" style={{ height: "56vh", alignItems: "center" }}>
+              <Card.Body>
+                <Card.Header style={{width: "100%"}}>
+                  <Card.Title>{projData[index].title}</Card.Title>
+                </Card.Header>
+                <Card.Text>{projData[index].projectDesc}</Card.Text>
+              </Card.Body>
+              <Row
+                className="justify-content-between"
+                style={{ bottom: "10px" }}
+              >
                 <Col className="d-inline-flex p-2 my-2">
-                  <Button className={!projData[index].url ? "disabled": ""} style={{width: "100%"}}href={projData[index].url}>Deployed Site</Button>
+                  <Button
+                    className={!projData[index].url ? "disabled" : ""}
+                    style={{ width: "100%" }}
+                    href={projData[index].url}
+                  >
+                    Deployed Site
+                  </Button>
                 </Col>
                 <Col className="d-inline-flex p-2 my-2">
-                  <Button style={{width: "100%"}}href={projData[index].git} >GitHub</Button>
+                  <Button style={{ width: "100%" }} href={projData[index].git}>
+                    GitHub
+                  </Button>
                 </Col>
               </Row>
-            <Alert style={{bottom: "10px", width: "80%"}}>Scroll Down For More<img src={down} style={{height :"20px", left: "10%"}}/></Alert>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-    <Container className="justify-content-center" style={{margin:"auto auto 20vh"}} fluid>
-        <h2 style={{background: "#496DDB", border: "solid 5px black"}}>All Projects</h2>
+              <Alert style={{ bottom: "10px", width: "80%" }}>
+                Scroll Down For More
+                <img src={down} style={{ height: "20px", left: "10%" }} />
+              </Alert>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Container
+        className="justify-content-center"
+        style={{ margin: "auto auto 20vh" }}
+        fluid
+      >
+        <h2 style={{ background: "#496DDB", border: "solid 5px black" }}>
+          All Projects
+        </h2>
         {renderCards()}
-    </Container>
-    
-</>
-     
-        
-
-    
+      </Container>
+    </>
   );
 }
 
--- wills-react-portfolio/src/pages/Resume.js
+++ wills-react-portfolio/src/pages/Resume.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Button, Container, Row, Col } from "react-bootstrap";
 
+
 function Resume(){
 
     return (
-        <Row className="top-50">
+        <Row style={{marginTop: "35vh",  }}>
             <Col>
-            <Button className="top-50">TEST</Button>
+            <h2 style={{marginBottom:'40px'}}>Click below to view/download my resume!</h2>
+            <Button href="https://docs.google.com/document/d/1lFRkE5YcmBWq92EbrwdYNRiIRqi491jTU2YMrqXZpko/edit?usp=sharing" variant="outline-primary" style={{width:'20%', height:'150%', fontSize:"200%"}} download>Download</Button>
+            <Button href='https://docs.google.com/document/d/1lFRkE5YcmBWq92EbrwdYNRiIRqi491jTU2YMrqXZpko/edit?usp=sharing' variant="outline-success" style={{width:'20%', height:'150%', fontSize:"200%"}}>View on Google Docs</Button>
             </Col>
+           
+        
             
         </Row>
         
