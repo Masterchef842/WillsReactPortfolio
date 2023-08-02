9a6e8e3b8f1dc754b41853406b9548fe2fefbb8c
Masterchef842 <robertwilliamlord55@gmail.com>
2023-08-01 19:40:08 -0500

    added contact form

--- wills-react-portfolio/src/pages/Contact.js
+++ wills-react-portfolio/src/pages/Contact.js
@@ -1,11 +1,106 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
+import emailjs from "emailjs-com";
 
+export default function Contact() {
+  const form = useRef();
+  const [user, setUser]=useState('');
+  const [userEmail, setEmail]=useState('');
+  const [userMessage, setMessage]=useState('');
+  const [displayErr, setErr]=useState( false)
 
-function Contact(){
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    return (
-        <h1 className="test">TEST 2</h1>
-    );
+    if(user && userMessage && /^(?:(?!.*?[.]{2})[a-zA-Z0-9](?:[a-zA-Z0-9.+!%-]{1,64}|)|\"[a-zA-Z0-9.+!% -]{1,64}\")@[a-zA-Z0-9][a-zA-Z0-9.-]+(.[a-z]{2,}|.[0-9]{1,})$/.test(userEmail)){
+        setErr(false)
+          emailjs
+      .sendForm(
+        "service_ke9fl14",
+        "template_jet9z4m",
+        form.current,
+        "sBAIHVIqVWTZAloOn"
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+         
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    
+      setEmail('')
+      setUser('')
+      setMessage('')
+      e.target.reset()
+    }
+    else{
+        setErr(true)
+    }
+    
+  };
+
+  const handleFormChange=(e)=>{
+    if(e.target.name==="from_name")
+        setUser(e.target.value)
+    if(e.target.name==="reply_to" )
+        setEmail(e.target.value)
+    if(e.target.name==="message")
+        setMessage(e.target.value)
+
+    if(e.target.value=""){
+
+    }
+
+    
+
+  }
+
+  return (
+    <Container>
+      <Row style={{ marginTop: "20vh" }}>
+        <Col>
+          <Card bg="light" text="dark" border="primary">
+            <Card.Header>Contact Form</Card.Header>
+
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              style={{ width: "80%", alignSelf: "center", margin: "10%" }}
+            >
+              <Row style={{ margin: "0 5% 5%" }}>
+                <label>Name</label>
+                <input type="text" name="from_name" onChange={(e)=>{handleFormChange(e)}} value={user}/>
+                <span style={displayErr && !user? {color:'red'} : {display: 'none'}}>Name is required</span>
+              </Row>
+              <Row style={{ margin: "5%" }}>
+                <label>Email</label>
+                <input type="email" name="reply_to" onChange={(e)=>{handleFormChange(e)}} value={userEmail}/>
+                <span style={displayErr && !userEmail? {color:'red'} : {display: 'none'}}>Valid email is required</span>
+                
+              </Row>
+
+              <Row style={{ margin: "5%"}}>
+                <label>Message</label>
+                <textarea name="message"  onChange={(e)=>{handleFormChange(e)}} value={userMessage}/>
+                <span style={displayErr && !userMessage? {color:'red'} : {display: 'none'}}>Message is required</span>
+              </Row>
+              <input type="submit" value="Send" />
+            </form>
+          </Card>
+        </Col>
+        <Col>
+          <h3 style={{marginTop: "20%"}}>
+            <p>
+              Need a website built or a position filled at you company? Leave a
+              message and your email address in the contact form, and Ill reach back out to you as soon
+              as possible!
+            </p>
+          </h3>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-
-export default Contact;
--- wills-react-portfolio/src/pages/Resume.js
+++ wills-react-portfolio/src/pages/Resume.js
@@ -7,7 +7,7 @@ function Resume(){
     const downloadFile=(url)=>{
         const aTag=document.createElement('a');
         aTag.href=url;
-        aTag.setAttribute('download','Robert_Lord_Resume/pdf');
+        aTag.setAttribute('download','Robert_Lord_Resume.pdf');
         document.body.appendChild(aTag);
         aTag.click();
         aTag.remove();
@@ -19,7 +19,7 @@ function Resume(){
             <Col>
             <h2 style={{ background: "#496DDB", margin: "20vh 10%", border: "solid 5px black", alignSelf:"center" }}>Downloads Center</h2>
             <h3 style={{marginBottom:'40px'}}>Click below to view/download my resume!</h3>
-            <Button href="https://docs.google.com/document/d/1lFRkE5YcmBWq92EbrwdYNRiIRqi491jTU2YMrqXZpko/edit?usp=sharing" variant="outline-primary" style={{width:'20%', height:'30%', fontSize:"200%"}} onClick={()=>{downloadFile(PDFurl)}} >Download</Button>
+            <Button variant="outline-primary" style={{width:'20%', height:'30%', fontSize:"200%"}} onClick={()=>{downloadFile(PDFurl)}} >Download</Button>
             <Button href='https://docs.google.com/document/d/1lFRkE5YcmBWq92EbrwdYNRiIRqi491jTU2YMrqXZpko/edit?usp=sharing' variant="outline-success" style={{width:'20%', height:'30%', fontSize:"200%"}}>View on Google Docs</Button>
             </Col>
            
